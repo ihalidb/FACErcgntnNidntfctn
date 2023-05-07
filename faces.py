a01e83932d64f827d9333b2ebfb196cfacd0753f
ihalidb <ibrahimh.bayrak@gmail.com>
2023-05-07 18:55:42 +0300

    görüntüye isim eklendi

--- faces.py
+++ faces.py
@@ -30,6 +30,11 @@ while True:
         if 45 >= conf <= 85:
             print(id_)
             print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
 
         img_item = "my-image.png"
         cv2.imwrite(img_item, roi_gray)
