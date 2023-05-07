8029099c5843e5fbac6731bcf5312ea9d5ab02b2
ihalidb <ibrahimh.bayrak@gmail.com>
2023-05-07 18:16:25 +0300

    train hazırlandı.

--- faces-train.py
+++ faces-train.py
@@ -3,12 +3,14 @@ import cv2
 import os
 import numpy as np
 from PIL import Image
+import pickle
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
-
 cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_alt2.xml"
 face_cascade = cv2.CascadeClassifier(str(cascade_path))
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
 
 current_id = 0
 label_ids = {}
@@ -20,17 +22,17 @@ for root, dirs, files in os.walk(image_dir):
         if file.endswith("png") or file.endswith("jpg"):
             path = os.path.join(root, file)
             label = os.path.basename(root).replace(" ", "-").lower()
-            print(label, path)
+            #print(label, path)
             if not label in label_ids:
                 label_ids[label] = current_id
                 current_id += 1
             id_ = label_ids[label]
-            print(label_ids)
+            #print(label_ids)
             # y_labels.append(label)  #numaraları_kaydet
             # x_train.append(path)  #resimleri_onayla_ve_NUMPY_dizisine_çevir
             pil_image = Image.open(path).convert("L")  # gray scale
             image_array = np.array(pil_image, "uint8")
-            print(image_array)
+            #print(image_array)
             faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
 
             for (x, y, w, h) in faces:
@@ -38,5 +40,11 @@ for root, dirs, files in os.walk(image_dir):
                 x_train.append(roi)
                 y_labels.append(id_)
 
-print(y_labels)
-print(x_train)
+# print(y_labels)
+# print(x_train)
+
+with open("labels.picle", 'wb') as f:
+    pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("trainer.yml")
