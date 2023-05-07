d2148c43b25c8926ea207da18e8fda2a36d8c2a5
ihalidb <ibrahimh.bayrak@gmail.com>
2023-05-07 16:22:43 +0300

    add rectangle to face-rec

--- facess.py
+++ facess.py
@@ -11,12 +11,12 @@ while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     face = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-    for(x,y,w,h)
+    for(x,y,w,h) in face
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         img_item = "my-image.png"
         cv2.imwrite(img_item, roi_gray)
-        color = (255,0,0) #BGR
+        color = (255,0,0) #BGR:
         stroke = 2
         end_cord_x = x + w
         end_cord_y = y + h
