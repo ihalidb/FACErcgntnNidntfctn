e1be76257c4b3f7c39b16982e75f7acaed7a3b16
ihalidb <ibrahimh.bayrak@gmail.com>
2023-05-07 17:56:54 +0300

    train için ön hazırlık isimleri ekle

--- faces-train.py
+++ faces-train.py
@@ -1,10 +1,17 @@
+import pathlib
+import cv2
 import os
-
 import numpy as np
 from PIL import Image
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
 
+cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_alt2.xml"
+face_cascade = cv2.CascadeClassifier(str(cascade_path))
+
+current_id = 0
+label_ids = {}
 y_labels = []
 x_train = []
 
@@ -14,8 +21,22 @@ for root, dirs, files in os.walk(image_dir):
             path = os.path.join(root, file)
             label = os.path.basename(root).replace(" ", "-").lower()
             print(label, path)
-            # y_labels.append(label)  # bir kaç numara
-            # x_train.append(path)  # resimleri onayla ve NUMPY dizisine çevir
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
+            id_ = label_ids[label]
+            print(label_ids)
+            # y_labels.append(label)  #numaraları_kaydet
+            # x_train.append(path)  #resimleri_onayla_ve_NUMPY_dizisine_çevir
             pil_image = Image.open(path).convert("L")  # gray scale
             image_array = np.array(pil_image, "uint8")
             print(image_array)
+            faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+
+            for (x, y, w, h) in faces:
+                roi = image_array[y:y + h, x:x + w]
+                x_train.append(roi)
+                y_labels.append(id_)
+
+print(y_labels)
+print(x_train)
