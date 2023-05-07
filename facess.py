fc8c51213642d4fbe3c42445fe07f5ca44a3bdf9
ihalidb <ibrahimh.bayrak@gmail.com>
2023-05-07 16:19:36 +0300

    add rectangle to face-rec

--- facess.py
+++ facess.py
@@ -1,11 +1,26 @@
 import numpy as np
 import cv2
 
+face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_alt2.xml/')
+
+
 cap = cv2.VideoCapture(0)
 
 while(True):
     #Capture frame-by-frame
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_gray)
+        color = (255,0,0) #BGR
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
 
     #Dispay the resulting frame
     cv2.imshow('frame', frame)
